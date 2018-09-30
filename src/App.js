00991888a09b0b39dcfe1139b6a02223197a0fa7
Hope R <nadezhda.lkn@yahoo.com>
2018-09-30 14:50:45 +0300

    Add input handler

--- src/App.js
+++ src/App.js
@@ -3,19 +3,47 @@ import './App.css';
 
 const numbers = [ 1, 2, 3, '*', 4, 5, 6, '/', 7, 8, 9, '-', '.', 0, '=', '+' ]
 
-function Button(props) {
+class Button extends Component {
+  render() {
   return (
-    <button className="button" value={props.value}>{props.value}</button>
-  )
+    <button className="button" value={this.props.value} onClick={this.props.onClick}>{this.props.value}</button>
+  )}
 }
 
-
+const regexInput = /\.?\d/
 class App extends Component {
+  state = {
+    input: '',
+    result: 0,
+    number1: 0,
+    number2: 0
+  }
+
+  handleClick = (event) => {
+    console.log(event.target.value)
+    if (event.target.value.match(regexInput)) {
+      this.setState({
+        input: this.state.input + event.target.value
+      })
+    }
+  }
+
+
   render() {
     return (
       <div className="container">
-      <h3 className="result"></h3>
-      {numbers.map(item => <Button value={item} />)}
+          <h3 className="result">
+              {this.state.input}
+          </h3>
+          {numbers.map(item => 
+
+                        <Button 
+                          key={item} 
+                          className="button" 
+                          value={item} 
+                          onClick={this.handleClick}
+                        />
+          )}
       </div>
     );
   }
