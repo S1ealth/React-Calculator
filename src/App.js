ccf1d466198a770eb3f9b1cbba3cc864d9d5e2be
Hope R <nadezhda.lkn@yahoo.com>
2018-09-30 14:03:52 +0300

    Add basic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const numbers = [ 1, 2, 3, '*', 4, 5, 6, '/', 7, 8, 9, '-', '.', 0, '=', '+' ]
+
+function Button(props) {
+  return (
+    <button className="button" value={props.value}>{props.value}</button>
+  )
+}
+
+
 class App extends Component {
   render() {
     return (
       <div className="container">
+      <h3 className="result"></h3>
+      {numbers.map(item => <Button value={item} />)}
       </div>
     );
   }
